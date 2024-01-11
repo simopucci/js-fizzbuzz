078ca0ded626c793bf8a86fd69cf301c11c2fcc3
simopucci <simo.pucci7@gmail.com>
2024-01-11 14:55:49 +0100

    feat: add JavaScript function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
+const cardsRow = document.getElementById('cards-row');
+
+
 // * for loop
 for (let i = 1; i <= 100; i++) {
+    let cardText = '';
+    let cardClass = '';
 
     if (i % 15 == 0) {
-        console.log('fizzbuzz');
+        cardText = 'fizzbuzz';
+        cardClass = 'bg-success';
     } else if (i % 5 == 0) {
-        console.log('buzz');
+        cardText = 'buzz';
+        cardClass = 'bg-danger';
     } else if (i % 3 == 0) {
-        console.log('fizz');
+        cardText = 'fizz';
+        cardClass = 'bg-primary';
     } else {
-        console.log(i);
+        cardText = i;
     }
-
+    
+    // * add cards
+    cardsRow.innerHTML += `
+    <div class="col-2 g-3">
+        <div class="card ${cardClass}">
+            <h3>${cardText}</h3>
+        </div>
+    </div>
+    `;
 }
